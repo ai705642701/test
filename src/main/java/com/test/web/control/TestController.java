8da21d80492c79685a070df9294599fd4c87ff88
caochengwu <ai705642701@163.com>
2018-07-09 10:19:42 +0800

    111

---
+++ src/main/java/com/test/web/control/TestController.java
@@ -0,0 +1,18 @@
+package com.test.web.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by caochengwu on 2018/6/29.
+ */
+
+@Controller
+public class TestController {
+
+    @RequestMapping("ccw1")
+    public String test(){
+
+        return "test";
+    }
+}
---
+++ src/main/java/otherTest/CodeBlock02.java
@@ -0,0 +1,18 @@
+package otherTest;
+
+/**
+ * Created by caochengwu on 2018/7/9.
+ */
+public class CodeBlock02 {
+
+    {
+        System.out.println("第一代码块");
+    }
+
+
+    public static void main(String[] args) {
+        new CodeBlock02();
+        new CodeBlock02();
+    }
+
+}
